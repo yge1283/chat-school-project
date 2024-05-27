a998d8f98dbd259c8a2acce50f86b2d5a9f3fca5
Cuunlang <zxcvbnm1131303@gmail.com>
2024-05-27 15:58:12 +0900

    etc:가이드용 주석 추가

--- dbconn/__init__.py
+++ dbconn/__init__.py
@@ -11,16 +11,32 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    conn = Connector(db_uri) #app.ini에서 db주소 가져와 객체 생성
+    conn.connect()           #db와 연결 (프로그램 실행시 최초1회만)
+
     db = SQLAlchemy()
     migrate = Migrate()
     db.init_app(app)
     migrate.init_app(app, db)
-    from . import models
 
+    # views에 Blueprint 만든 후 꼭 연결해주기!!!!!!!!!!!!!!!!
+     
     from .views import student_main, chatbot
-    #app.register_blueprint(login.bp)
+    #app.register_blueprint(login.bp) # login은 별도 api사용하니 구현 다 되면 연결해주세요
     app.register_blueprint(student_main.bp)
     app.register_blueprint(chatbot.bp)
 
 
     return app
+
+"""
+블루프린트 연결 가이드
+1. from .views import ~ 에서 본인이 만든 views폴더안 .py파일의 이름을 추가해 주세요.
+2. 다음 줄에 app.register_blueprint(<본인이선언한 bp함수명>.bp)
+     본인이 선언한 bp이름은 views폴더안에 본인이 작업하는(만든)파일 들어가서
+      예시로 bp = Blueprint('chatbot', __name__, url_prefix='/edu_chatbot')
+                            ^^^^^^^
+                            이 이름을 적어주세요.
+                            헷갈리지 않을려면 .py파일이름과 같게 하셔도 됨
+
+"""
