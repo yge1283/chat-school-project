0343b1808f9735fa0f778db1d6f090892152727c
ibo12224 <ibo12224@naver.com>
2024-05-28 17:44:10 +0900

    docs chat.py설명 추가

--- dbconn/views/chat.py
+++ dbconn/views/chat.py
@@ -3,7 +3,7 @@ from flask_socketio import emit
 from datetime import datetime
 from .. import conn,socketio  # 여기서는 모듈 간의 의존성을 최소화합니다.
 bp = Blueprint('chat', __name__, url_prefix='/chat')
-uid = ""
+uid = "082d8640-9287-4284-9a73-47543b255309"
 
 @bp.route('/')
 def show_chatbot_page():
@@ -21,5 +21,9 @@ def handle_message(data):
 def post_message(data):
     message = data['msg']
     nowdate = datetime.now()
-    new_chat = [(1, "082d8640-9287-4284-9a73-47543b255309", nowdate, message, "")]
+    new_chat = [(1, uid, nowdate, message, "")]
     conn.tb_ninsert("Chat", new_chat)
+
+# http://127.0.0.1:5000/chat/
+# 위 코드로 접속시 send할 경우 보내는 짐
+#기존 html 선행 조건: 본인 uid를 받는다는 전재로 작동됨, 추가 설치(flask_socketio)
