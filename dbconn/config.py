0a4292bc22a004d145f95ad6efe3d52f0e3886bb
unknown <yge1283>
2024-06-14 15:05:38 +0900

    슈퍼베이스 키 지움

--- dbconn/config.py
+++ dbconn/config.py
@@ -2,4 +2,4 @@
 # : config.py
 
 SUPABASE_URL = "https://bbgqkwpyhnlksydowvtp.supabase.co"
-SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiZ3Frd3B5aG5sa3N5ZG93dnRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2NTI1MTUsImV4cCI6MjAyODIyODUxNX0.BY1GJw2Dm-iqW2lh8Fo37WJpxyH4E-gKtBbPmV2p0Os' 
+SUPABASE_KEY = '추가' 
