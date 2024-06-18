9c05c6c06d91c680ce290f785c13daf3c1829d59
Cuunlang <zxcvbnm1131303@gmail.com>
2024-06-18 18:37:26 +0900

    rm cache config.py

--- dbconn/config.py
+++
@@ -1,5 +0,0 @@
-# .gitignore 파일에 다음과 같이 추가합니다.
-# : config.py
-
-SUPABASE_URL = "https://bbgqkwpyhnlksydowvtp.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiZ3Frd3B5aG5sa3N5ZG93dnRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2NTI1MTUsImV4cCI6MjAyODIyODUxNX0.BY1GJw2Dm-iqW2lh8Fo37WJpxyH4E-gKtBbPmV2p0Os" 
